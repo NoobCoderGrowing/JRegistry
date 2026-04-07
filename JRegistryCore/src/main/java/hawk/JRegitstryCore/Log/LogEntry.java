ed4cb11aa7e7e657626548586b5a5d3122b39357
wenjun <wenjun.yao@uqconnect.com.au>
2026-04-07 23:03:53 +1000

    implement logEntry

---
+++ JRegistryCore/src/main/java/hawk/JRegitstryCore/Log/LogEntry.java
@@ -0,0 +1,14 @@
+package hawk.JRegitstryCore.Log;
+
+import lombok.Data;
+
+@Data
+public class LogEntry {
+    
+    private long term;
+    private long index;
+    private String command;
+    private String key;
+    private byte[] value;
+    
+}
