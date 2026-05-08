31513ed553b11be7de8c170f656cd7956ff91212
wenjun <wenjun.yao@uqconnect.com.au>
2026-05-08 12:45:14 +1000

    restructure project

--- JRegistryCenter/src/main/java/hawk/JRegistryCenter/Config/RaftConfig.java
+++ JRegistryCenter/src/main/java/hawk/JRegistryCenter/Config/RaftConfig.java
@@ -1,4 +1,4 @@
-package hawk.JRegistryCenter.Raft.Config;
+package hawk.JRegistryCenter.Config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
